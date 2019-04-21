7c1b4fababdce7b21bd6d07736fb5e0ee827a743
Emmet <emmetlawton@gmail.com>
2019-04-21 13:55:57 +0100

    fixed the http-service.js file

--- src/Services/http-service.js
+++ src/Services/http-service.js
@@ -4,11 +4,11 @@ import { resolve } from 'dns';
 class HttpService{
     getProducts = () => {
         var promise = new Promise((resolve, reject) => {
-
+            fetch('http://localhost:3004/product').then(response => {
+                console.log(response.json());
+            });
         });
-        fetch('http://localhost:3004/product').then(response => {
-            console.log(response.json());
-        });
+       return promise;
     }
 
    
