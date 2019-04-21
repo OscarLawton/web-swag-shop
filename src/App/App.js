f6f6eddc917adfba837b5c1076bc739cee0253ee
Emmet <emmetlawton@gmail.com>
2019-04-21 21:46:00 +0100

    fixed bugs, and added cards for products

--- src/App/App.js
+++ src/App/App.js
@@ -5,9 +5,21 @@ import HttpService from "../Services/http-service"
 import Product from "../product/product"
 const http = new HttpService();
 class App extends Component {
+
   constructor(props){
+
     super(props);
-    http.getProducts();
+    this.loadData = this.loadData.bind(this);
+    this.loadData();
+  
+  }
+
+  loadData = () =>{
+    http.getProducts().then(products => {
+      console.log(products);
+    }, err => {
+      console.log(err);
+    });
   }
 
   render() {
@@ -15,10 +27,15 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-         
+          <h4>The Swag Shop</h4>
         </header>
-        <div className="App-main">
-            <Product />
+        <div className="container App-main">
+            <div className="row">
+            <Product price="5.99" title="cool toy gun" imgURL="https://ae01.alicdn.com/kf/HTB1NkpTaPihSKJjy0Fiq6AuiFXa4/Colorful-Cool-Toy-Gun-With-Soft-Bullets-The-Best-Toys-for-Children-as-the-Gift-Wonderful.jpg_640x640.jpg"/>
+            <Product price="5.99" title="cool toy gun" imgURL="https://ae01.alicdn.com/kf/HTB1NkpTaPihSKJjy0Fiq6AuiFXa4/Colorful-Cool-Toy-Gun-With-Soft-Bullets-The-Best-Toys-for-Children-as-the-Gift-Wonderful.jpg_640x640.jpg"/>
+            <Product price="5.99" title="cool toy gun" imgURL="https://ae01.alicdn.com/kf/HTB1NkpTaPihSKJjy0Fiq6AuiFXa4/Colorful-Cool-Toy-Gun-With-Soft-Bullets-The-Best-Toys-for-Children-as-the-Gift-Wonderful.jpg_640x640.jpg"/>
+            </div>
+           
         </div>
       </div>
     );
--- src/Services/http-service.js
+++ src/Services/http-service.js
@@ -1,11 +1,13 @@
 import 'whatwg-fetch'
-import { resolve } from 'dns';
+
 
 class HttpService{
     getProducts = () => {
         var promise = new Promise((resolve, reject) => {
             fetch('http://localhost:3004/product').then(response => {
                 console.log(response.json());
+                resolve(response.json());
+                reject("something went wrong :o");
             });
         });
        return promise;
